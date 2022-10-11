1f61547f23ecdbcc6d0e526e8b83b88e3dde5c9d
haku-c <kokoanimal@gmail.com>
2022-10-11 19:05:45 -0400

    Create functions for different color transitions
    - Remove padding on quote sources
    - Remove scrub on scroll animations

--- projects/scroll.js
+++ projects/scroll.js
@@ -1,15 +1,42 @@
-const shift_colors = gsap.to("body", {
-  backgroundColor: "#7bdcb5",
-  "--text-color": "#fcf5f5",
-  duration: 3,
-  ease: Power3.easeOut,
-});
+var timeline = gsap.timeline();
+
+function shift_color(background_color, text_color) {
+  return gsap.to("body", {
+    backgroundColor: background_color,
+    "--text-color": text_color,
+    ease: Power3.easeOut,
+  });
+}
+
+const shift_colors_teal = shift_color("#7bdcb5", "#fcf5f5");
+
+const shift_colors_purple = shift_color("#dccbfc", "#0c033f");
+
+const shift_colors_red = shift_color("#b71313", "#fffd92");
 
 ScrollTrigger.create({
-  trigger: "#first-trigger",
+  trigger: "#teal-trigger",
   start: "top center",
   end: "top 300px",
   markers: true,
-  scrub: true,
-  animation: shift_colors,
+  // scrub: true,
+  animation: shift_colors_teal,
+});
+
+ScrollTrigger.create({
+  trigger: "#purple-trigger",
+  start: "top center",
+  end: "top 300px",
+  markers: true,
+  // scrub: true,
+  animation: shift_colors_purple,
+});
+
+ScrollTrigger.create({
+  trigger: "#red-trigger",
+  start: "top center",
+  end: "top 300px",
+  markers: true,
+  // scrub: true,
+  animation: shift_colors_red,
 });
